5e3e4e1191f3716616e25377be699a8b31455a91
PankajBengaluru <pankaj.xworkz@gmail.com>
2024-08-25 10:55:02 +0530

    C:/Program Files/Git/

---
+++ Bangalore.java
@@ -0,0 +1,18 @@
+public class Bangalore{
+	public static void temp(double deg, String Place){
+		System.out.println("Hi, this is Class Bangalore");
+
+	
+	//1.
+	if(double temp == 66.7 && String place =="Rajajinagar" ){
+		System.out.println("This is if condition for && Operation");
+		}
+		else{
+			System.out.println("This is else condition & did not satisfy the condition");
+	}
+	}
+	
+}
+
+
+
---
+++ BangaloreRunner.java
@@ -0,0 +1,6 @@
+class BangaloreRunner{
+	public static void main(String [] args){
+		Bangalore.temp(45, "Vijayanagar");
+
+	}
+}
